7d09418a99aa8fcc075e40b06206322b09f64221
c-c-k <c-c-k@nym.hush.com>
2026-02-21 00:33:57 +0200

    feat(rust): sum-of-multiples

---
+++ rust/sum-of-multiples/iteration_final/src/lib.rs
@@ -0,0 +1,21 @@
+use std::collections::HashSet;
+
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    (1..limit)
+        .filter(|num| {
+            factors
+                .iter()
+                .any(|&factor| factor != 0 && num % factor == 0)
+        })
+        .sum()
+}
+
+pub fn sum_from_factors(limit: u32, factors: &[u32]) -> u32 {
+    factors
+        .iter()
+        .filter(|&&factor| factor != 0)
+        .flat_map(|&factor| (factor..limit).step_by(factor as usize))
+        .collect::<HashSet<_>>()
+        .iter()
+        .sum()
+}
